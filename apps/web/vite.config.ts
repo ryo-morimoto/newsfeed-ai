15a4f47248314a22b31affb1b68ba0b20c807b40
ryo-morimoto <ryo.morimoto.dev@gmail.com>
2026-01-25 17:05:44 +0900

    chore(web): configure CSS and Islands build

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,14 +3,36 @@ import devServer from "@hono/vite-dev-server";
 import build from "@hono/vite-build/cloudflare-workers";
 import UnoCSS from "unocss/vite";
 
-export default defineConfig({
-  plugins: [
-    UnoCSS(),
-    build({
-      entry: "src/index.tsx",
-    }),
-    devServer({
-      entry: "src/index.tsx",
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      plugins: [UnoCSS()],
+      build: {
+        outDir: "dist/assets/static",
+        rollupOptions: {
+          input: {
+            style: "./src/styles/main.css",
+            islands: "./src/islands/index.tsx",
+          },
+          output: {
+            entryFileNames: "[name].js",
+            assetFileNames: "[name].[ext]",
+          },
+        },
+      },
+    };
+  }
+
+  return {
+    plugins: [
+      UnoCSS(),
+      build({
+        entry: "src/index.tsx",
+        output: "dist/index.js",
+      }),
+      devServer({
+        entry: "src/index.tsx",
+      }),
+    ],
+  };
 });
